0c8f840032dba3075f1bbd68f410b5995e87ad2e
dadaxon1106 <dadaxonmakh@gmail.com>
2023-11-05 12:31:19 +0500

    change main page

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,12 @@
 // import 'package:begginer_widgets_homework/mainpage/main_view.dart';
 // import 'package:begginer_widgets_homework/singerspage/singers_view.dart';
 // import 'package:begginer_widgets_homework/widgetspage/widgets_page.dart';
+import 'package:begginer_widgets_homework/actorspage/actor_view.dart';
+import 'package:begginer_widgets_homework/famouspersonspage/famous_persons_page.dart';
+import 'package:begginer_widgets_homework/footballpaleyerspage/football_players_page.dart';
 import 'package:begginer_widgets_homework/mainpage/main_view.dart';
-import 'package:begginer_widgets_homework/widgetspage/widgets_page.dart';
+import 'package:begginer_widgets_homework/singerspage/singers_view.dart';
+// import 'package:begginer_widgets_homework/widgetspage/widgets_page.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -19,7 +23,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      home: const MainPage(),
+      initialRoute: MainPage.route,
+      routes: {
+        MainPage.route: (context) => const MainPage(),
+        SingerPage.route: (context) => const SingerPage(),
+        FamousPage.route: (context) => const FamousPage(),
+        ActorPage.route: (context) => const ActorPage(),
+        FootballPlayersPage.route: (context) => const FootballPlayersPage(),
+      },
     );
   }
 }
